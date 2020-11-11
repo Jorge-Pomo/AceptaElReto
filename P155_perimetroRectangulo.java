ead514b0bc6c22e5b99bb38a4fc63685ff6201b3
Jorge-Pomo <jorge.pomo.sgpl@gmail.com>
2020-11-11 09:41:45 +0100

    acepta el reto 1

---
+++ P155_perimetroRectangulo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class P155_perimetroRectangulo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		//VARIABLES
+		
+		int base = 0;
+		int altura = 0;
+		
+		//ALGORITME
+		
+		base = sc.nextInt();
+		altura = sc.nextInt();
+		
+		do {
+			
+		}while(base <= 0 && altura <= 0);
+		
+	}
+
+}
