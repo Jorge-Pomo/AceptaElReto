296406ccbbca81caee8b679951bd3f3d6aa14387
Jorge-Pomo <jorge.pomo.sgpl@gmail.com>
2020-12-04 12:35:44 +0100

    ns

---
+++ P180_Triangulos.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class P180_Triangulos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		
+		//VARIABLES
+		
+		int cont;
+		int cost1;
+		int cost2;
+		int cost3;
+		int resu;
+		
+		//ALGORITME
+		
+		cont = sc.nextInt();
+		
+		for (int i = 0; i < cont; i++) {
+			cost1 = sc.nextInt();
+			cost2 = sc.nextInt();
+			cost3 = sc.nextInt();
+			
+			if(cost1 > 0 && cost2 > 0 && cost3 > 0) {
+				
+				//Costado 1
+				
+			}
+		}
+	
+	}
+
+}
